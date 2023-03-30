08809e8e2df9a93d96e59c4e42eb82caffc5a923
hassankh0 <hassan.h.khalife@outlook.com>
2023-03-30 23:24:13 +0200

    cours table & cours api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CoursController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['cors'])->group(function () {
+    Route::get('/cours', [CoursController::class, "index"]); // List Courses
+    Route::post('/cours', [CoursController::class, "store"]); // Create Cours
+    Route::get('/cours/{cours}', [CoursController::class, "show"]); // Detail of Cours
+    Route::put('/cours/{cours}', [CoursController::class, "update"]); // Update Cours
+    Route::delete('/cours/{cours}', [CoursController::class, "destroy"]); // Delete Cours
 });
