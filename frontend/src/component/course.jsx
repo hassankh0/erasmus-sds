b686073c7f5c4c537c91ada14f8c62af87e3d44e
yodalesage29 <kalvin0803@hotmail.fr>
2023-04-03 20:46:06 +0200

    V1.1 : modification to university +add city.coutry

--- frontend/src/component/course.jsx
+++ frontend/src/component/course.jsx
@@ -12,11 +12,20 @@ function Course(props) {
     {/* Not HOVERING */}
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.CourseInfo.name}</h5>
     <p className="font-normal text-gray-700 dark:text-gray-400">{props.CourseInfo.description}</p>
+    <div class="h-6">
+      <div class="absolute bottom-5 right-8">
+        <p className="font-bold text-white dark:text-white-000">{props.CourseInfo.country}, <span className="font-normal">{props.CourseInfo.city}</span></p>
+      </div>
+    </div>
+    {/* <div className="flex justify-end">
+        <p className="font-bold text-gray-700 dark:text-gray-400">{props.CourseInfo.country}</p>
+        <p className="pl-1 font-normal text-gray-700 dark:text-gray-400">{props.CourseInfo.city}</p>
+    </div> */}
 
     {/* HOVERING */}
     {isHovering && (
         <div className="absolute top-0 left-0 w-full h-full z-10">
-        <div className="w-full h-full bg-white" style={{clipPath:" polygon(0 100%, 100% 100%, 100% 25%, 0 34.5%", padding: "2rem"}}>
+        <div className="w-full h-full bg-white" style={{clipPath:" polygon(0 100%, 100% 100%, 100% 25%, 0 30.5%", padding: "2rem"}}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-500 dark:text-gray-800">{props.CourseInfo.name}</h5>
             <div className="text-center">
                 <p className="bg-gray-500 text-gray-900 dark:bg-gray-500 dark:text-white py-2 px-4 rounded-full inline-block mt-4 ">View course</p>
--- frontend/src/component/listofcourse.jsx
+++ frontend/src/component/listofcourse.jsx
@@ -6,12 +6,12 @@ export default function ListofCourse() {
 
  //state
  const [courses, setCourses] = useState([
-    {id : 1, name: "Mathematique", description: "A math class typically involves the study of mathematical concepts and techniques, and their application to solve problems in a structured and logical manner. "},
-    {id : 2, name: "Français", description: "A French class typically involves the study of the French language, including grammar, conjugation, reading and listening comprehension, as well as the production of written and oral texts."},
-    {id : 3, name: "Informatique", description: "A French class typically involves the study of the French language, including grammar, conjugation, reading and listening comprehension, as well as the production of written and oral texts."},
-    {id : 4, name: "Musique", description: "A French class typically involves the study of the French language, including grammar, conjugation, reading and listening comprehension, as well as the production of written and oral texts."},
-    {id : 5, name: "Italien", description: "A French class typically involves the study of the French language, including grammar, conjugation, reading and listening comprehension, as well as the production of written and oral texts."},
-    {id : 5, name: "Italien", description: "A French class typically involves the study of the French language, including grammar, conjugation, reading and listening comprehension, as well as the production of written and oral texts."}
+    {id : 1, name: "EILCO", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering. ", country: "France", city: "Calais"},
+    {id : 2, name: "Polytechnique Poznan", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.", country: "Poland", city: "Poznan"},
+    {id : 3, name: "Polytechnique Paris", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.", country: "France", city: "Paris"},
+    {id : 4, name: "ULCO", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.", country: "France", city: "Calais"},
+    {id : 5, name: "EILCO", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.", country: "France", city: "Calais"},
+    {id : 5, name: "EILCO", description: "Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.Ecole du Littoral Cote d'Opale, the obecjective of the school is to teach some form of engeneering.", country: "France", city: "Calais"}
 
   ])
 
