300b144144f21d0f14a14b942b9223ffc6937691
hassankh0 <hassan.h.khalife@outlook.com>
2023-04-20 14:55:09 +0200

    fix icons

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -27,12 +27,12 @@ const Login = () => {
                     <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
 
                     <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                clip-rule="evenodd" />
                         </svg>
-                        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+                        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Username" />
                     </div>
                     <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
