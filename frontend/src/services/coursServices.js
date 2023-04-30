82eeaf1cb34361764b073890d13b8a2917dddd8f
hassankh0 <hassan.h.khalife@outlook.com>
2023-04-30 08:40:08 +0200

    add course to cart

--- frontend/src/services/coursServices.js
+++ frontend/src/services/coursServices.js
@@ -4,7 +4,6 @@ import { API_URL } from '../config/environment';
 export const getCourses = async () => {
     try {
         const response = await axios.get(`${API_URL}/cours`);
-        console.table(response.data.courses);
         return response.data.courses;
     } catch (error) {
         console.error(error);
@@ -57,6 +56,20 @@ export const getCoursesOfStudent = async () => {
         const response = await axios.get(`${API_URL}/courses/student/${id}`);
 
         return response.data.courses;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const addCoursesToStudent = async (coursId) => {
+    const studentId = JSON.parse(sessionStorage.getItem("student")).id;
+    try {
+        const response = await axios.post(`${API_URL}/carts`, {
+            student_id: studentId,
+            cours_id: coursId
+        });
+
+        return response.data.course;
     } catch (error) {
         console.error(error);
     }
